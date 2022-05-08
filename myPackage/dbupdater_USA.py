a33afa9a87998210169d74629fdd775446fbbcd7
dyun <zkdlxkxks@nate.com>
2022-05-09 00:03:38 +0900

    add: dbupdater_usa

--- myPackage/dbupdater_USA.py
+++ myPackage/dbupdater_USA.py
@@ -45,13 +45,11 @@ class DBUpdater:
         self.conn.close()
 
     def read_krx_code(self):
-        """KRX로부터 상장기업 목록 파일을 읽어와서 데이터프레임으로 반환"""
-        url = 'http://kind.krx.co.kr/corpgeneral/corpList.do?method=' \
-              'download&searchType=13'
-        krx = pd.read_html(url, header=0)[0]
-        krx = krx[['종목코드', '회사명']]
-        krx = krx.rename(columns={'종목코드': 'code', '회사명': 'company'})
-        krx.code = krx.code.map('{:06d}'.format)
+        """nasdaq global select 목록 파일을 읽어와서 데이터프레임으로 반환, \
+            현재는 수동 다운, 차후 url을 읽어 파일을 다운받아 오는 것이 목표"""
+        krx = pd.read_csv('C:/Develop/finance/nasdaq_global_select.csv')
+        krx = krx[['Symbol', 'Name']]
+        krx = krx.rename(columns={'Symbol': 'code', 'Name': 'company'})
         return krx
 
     def update_comp_info(self):
