fc0e1be3a64190e48914c725b4e919a589517ff7
dyun <zkdlxkxks@nate.com>
2023-05-02 17:22:08 +0900

    fix: remove password

--- myPackage/볼랜더 + 삼중창 알림.py
+++ myPackage/볼랜더 + 삼중창 알림.py
@@ -7,7 +7,7 @@ from slacker import Slacker
 import ana2
 import analyzer
 
-# slack = Slacker('xoxb-3406314155365-3433339692400-R5cTypQNNwvuZodBILiljefB')
+# slack = Slacker('')
 
 
 # def dbgout(message):
@@ -23,7 +23,7 @@ def post_message(token, channel, text):
         data={"channel": channel,"text": text}
     )
  
-myToken = 'xoxb-3406314155365-3433339692400-R5cTypQNNwvuZodBILiljefB'
+myToken = ''
 def dbgout_sell(message):
     """인자로 받은 문자열을 파이썬 셸과 슬랙으로 동시에 출력한다."""
     print(datetime.now().strftime('[%m/%d %H:%M:%S]'), message)
